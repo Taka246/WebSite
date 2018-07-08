7830be0e09ac73f15569925e7d4b8e781fd979d5
mac <mac@macnoMacBook-puro.local>
2018-07-08 16:42:52 +0900

    add mysql

--- ramen/ramen/settings.py
+++ ramen/ramen/settings.py
@@ -76,6 +76,22 @@ WSGI_APPLICATION = 'ramen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'test',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
+#         'TEST': {
+#             'NAME': 'test_sample'
+#         }
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
